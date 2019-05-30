97a7855c9571de6fe550aee087556394f3a8febf
Rodrigo Moreira <rodrigomoreira07@gmail.com>
2019-05-30 00:02:48 -0300

    Added React Frontend

---
+++ react-front/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./MainRouter";
+
+const App = () => (
+  <BrowserRouter>
+    <MainRouter></MainRouter>
+  </BrowserRouter>
+)
+
+export default App;
